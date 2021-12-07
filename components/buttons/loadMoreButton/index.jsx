322d61e452cc9896f407845a1e80372f52e228a1
SamLCH <64434536+sam-vic@users.noreply.github.com>
2021-12-07 14:44:26 -0800

    small optimization of description code (help from Luigi)

--- components/buttons/loadMoreButton/index.jsx
+++ components/buttons/loadMoreButton/index.jsx
@@ -2,7 +2,7 @@ import Button from '..'
 
 export default function LoadMore() {
     return (
-        <Button height='50px' className='w-[300px] rounded-[30px] font-bold'>
+        <Button height='50px' className='w-[300px] rounded-[30px] font-bold my-[20px]'>
             Load More ...
         </Button>
     )
--- components/cards/index.jsx
+++ components/cards/index.jsx
@@ -1,27 +1,35 @@
 import styles from './Cards.module.css'
 import clsx from 'clsx'
 import { useQuery } from 'react-query'
+import _ from 'lodash'
+
+const isDescToLong = desc => {
+    const words = desc.trim().split(' ')
+
+    return words.length > 10
+}
 
 export default function Body() {
-    const cards_query = useQuery('CARDS', async () => await (await fetch('https://api.unsplash.com/photos/?client_id=rL_5c3JZU0epYcJVGrn1HHWpmFd_Uor1zirS2ZfHG9c&per_page=20')).json())
-    const info = cards_query.data || []
+    const { data: info = [] } = useQuery('CARDS', async () => await (await fetch('https://api.unsplash.com/photos/?client_id=rL_5c3JZU0epYcJVGrn1HHWpmFd_Uor1zirS2ZfHG9c&per_page=20&page=4')).json())
+
     console.log(info)
 
     return (
         <div className='flex flex-wrap justify-center'>
-
-            {info.map(({ urls: { regular }, user:{ first_name } }, index) =>
-                <div key={`card-${index}`} className='flex '>
-                    <Cards
-                        url='https://nextjs.org/docs'
-                        title='Documentation'
-                        text='Find in-depth information about Next.js features and API.'
-                        full={regular}
-                        first_name={first_name}
-                    />
-
-                </div>
-            )}
+            {info.map(({ urls: { regular }, user: { first_name, portfolio_url }, alt_description, description }, index) => {
+                const desc = description || alt_description || 'No Discription Available'
+                return (
+                    <div key={`card-${index}`} className='flex '>
+                        <Cards
+                            url={portfolio_url}
+                            title='Documentation'
+                            text={isDescToLong(desc) ? 'Click Through To Read More' : desc}
+                            full={regular}
+                            first_name={first_name}
+                        />
+                    </div>
+                )
+            })}
         </div>
     )
 }
@@ -30,16 +38,18 @@ export function Cards({ url, first_name, text, full, key }) {
     return (
         <a
             href={url}
-            className={clsx(styles.card, 'group relative overflow-hidden ')}
+            className={clsx(styles.card, 'group relative overflow-hidden min-w-[300px] min-h-[200px]')}
             key={key}
+            target='_blank'
+            rel="noopener"
         >
             <div className='block absolute w-full h-full group-hover:opacity-40 group-hover:scale-110 transition duration-500 ease-in-out' >
-                <img src={full} width={500} height={500} className='object-cover h-full w-full object-center'/>
+                <img src={full} width={500} height={500} className='object-cover h-full w-full object-center' />
             </div>
 
             <div className='opacity-0 group-hover:transition-opacity duration-300 ease-in-out group-hover:opacity-100 relative p-5'>
-                <h2>{first_name}'s Pictures &rarr; </h2>
-                <p>{text}</p>
+                <h2>{_.startCase(first_name)}'s Pictures &rarr; </h2>
+                <p className='max-w-[250px] h-[100px] line-clamp-3 '>{text}</p>
             </div>
         </a>
     )
